9b9caca6d1389d4a8b8a7796f6d92b5a5b0faa8c
Dmitry Mokrov <dmit.mokrov@yandex.ru>
2020-10-03 17:41:11 +0300

    Добавляет новые компоненты и роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Main from '../main/main';
+import Login from '../login/login';
+import Favorites from '../favorites/favorites';
+import Offer from '../offer/offer';
 
 const App = (props) => {
   const {offersCount} = props;
   return (
-    <Main offersCount={offersCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact>
+          <Main offersCount={offersCount}/>
+        </Route>
+        <Route path='/login' exact>
+          <Login/>
+        </Route>
+        <Route path='/favorites' exact>
+          <Favorites/>
+        </Route>
+        <Route path='/offer/:id?' exact>
+          <Offer/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
