b02742dcd33879ef8cd4b50c9d1c29f017f4c028
Dmitry Mokrov <dmit.mokrov@yandex.ru>
2020-10-11 15:44:52 +0300

    WIP Передает моки offers в компонент App через пропсы, добавляет валидацию пропсов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,7 +7,7 @@ import FavoritesPage from '../pages/favorites-page/favorites-page';
 import OfferPage from '../pages/offer-page/offer-page';
 
 const App = (props) => {
-  const {offersCount} = props;
+  const {offers, offersCount} = props;
   return (
     <BrowserRouter>
       <Switch>
@@ -24,6 +24,25 @@ const App = (props) => {
 };
 
 App.propTypes = {
+  offers: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        description: PropTypes.string,
+        type: PropTypes.oneOf([`Apartment`, `Private room`, `House`, `Hotel`]),
+        price: PropTypes.number,
+        rating: PropTypes.number,
+        isPremium: PropTypes.bool,
+        bedroomsCount: PropTypes.number,
+        guestsMaxCount: PropTypes.number,
+        features: PropTypes.arrayOf(PropTypes.string),
+        photos: PropTypes.arrayOf(PropTypes.string),
+        host: PropTypes.shape({
+          name: PropTypes.string,
+          avatar: PropTypes.string,
+          isSuper: PropTypes.bool
+        })
+      })
+  ).isRequired,
   offersCount: PropTypes.number.isRequired
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import {offers} from './mocks/offers';
 
 const offersCount = 312;
 
-ReactDOM.render(<App offersCount={offersCount}/>, document.querySelector(`#root`));
+ReactDOM.render(
+    <App
+      offers={offers}
+      offersCount={offersCount}
+    />,
+    document.querySelector(`#root`)
+);
