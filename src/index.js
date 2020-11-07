7e722af54ab2f3e8ae7c164b4e9f02c960bfeec9
Dmitry Mokrov <dmit.mokrov@yandex.ru>
2020-11-07 16:24:57 +0300

    Подключает api и thunk в приложение

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {offers} from './mocks/offers';
 import {reviews} from './mocks/reviews';
 import {reducer} from './store/reducer';
+import thunk from 'redux-thunk';
+import {createApi} from './services/api';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
+const api = createApi();
+
+// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
+
+const store = createStore(reducer, applyMiddleware(thunk.withExtraArgument(api)));
 
 ReactDOM.render(
     <Provider store={store}>
